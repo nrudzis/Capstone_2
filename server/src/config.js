e234dfea95042804af43b55adebaf4d23df493c4
Nick Rudzis <nick.rudzis@gmail.com>
2025-01-16 08:05:52 -0800

    install/configure bcrypt

--- server/src/config.js
+++ server/src/config.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 
 export const PORT = +process.env.PORT || 3001;
 
+export const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
 export function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
     ? "postgresql:///swap_test"
