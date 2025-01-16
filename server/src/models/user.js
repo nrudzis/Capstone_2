25e38fc0d14aa337c3f65adf483cc24f952ab833
Nick Rudzis <nick.rudzis@gmail.com>
2025-01-16 08:08:49 -0800

    add register method to User model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,9 +1,49 @@
-import db from "../db.js";
-import { NotFoundError } from "../expressError.js"
+import db from "../db.js"
+import { NotFoundError, BadRequestError } from "../expressError.js"
+import bcrypt from "bcrypt"
+import { BCRYPT_WORK_FACTOR } from "../config.js"
 
 /** Related functions for users. */
 
 class User {
+
+  /** Register a new user.
+   *
+   * Returns { username }
+   *
+   * Throws BadRequestError on duplicate usernames.
+   **/
+
+  static async register({ username, password }) {
+    const duplicateCheck = await db.query(
+      `SELECT username
+       FROM users
+       WHERE username = $1`,
+      [username],
+    );
+
+    if (duplicateCheck.rows[0]) {
+      throw new BadRequestError(`Username "${username}" is not available. Please try a different username.`);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users
+       (username,
+        password)
+       VALUES ($1, $2)
+       RETURNING username`,
+      [
+        username,
+        hashedPassword
+      ],
+    );
+
+    const user = result.rows[0];
+
+    return user;
+  }
 
   /** Get all the users.
    *
