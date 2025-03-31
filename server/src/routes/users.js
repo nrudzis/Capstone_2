d5472301b32d0f1d55ab0330294b48cff8f0019a
Nick Rudzis <nick.rudzis@gmail.com>
2025-03-30 21:04:58 -0700

    add route for market transactions

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -40,4 +40,20 @@ router.post("/:username/send-funds", async (req, res, next) => {
   }
 });
 
+/** User model check 5: market transaction */
+router.post("/:username/market-transaction", async (req, res, next) => {
+  try {
+    const marketTransactionDetails = {
+      username: req.params.username,
+      symbol: req.body.symbol,
+      orderType: req.body.orderType,
+      amount: req.body.amount
+    }
+    await User.marketTransaction(marketTransactionDetails);
+    return res.status(200).json({ message: "Transaction was successful." });
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = router;
