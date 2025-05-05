1a5e49b1ec138566c752c226bbc71f3351d45bf1
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-05 15:57:02 -0700

    add token POST route to auth routes, modify register route to create token

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,30 @@
 /** Routes for authentication */
 
+const { createToken } = require("../helpers/token.js");
+
 const express = require("express");
 const User = require("../models/user");
 
 const router = express.Router();
 
+/** User model check: authenticate */
+router.post("/token", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password);
+    const token = createToken(user);
+    return res.json({ token, username: user.username });
+  } catch (err) {
+    next(err);
+  }
+});
+
 /** User model check 3: register */
 router.post("/register", async (req, res, next) => {
   try {
     const user = await User.register(req.body);
-    return res.status(201).json({ user });
+    const token = createToken(user);
+    return res.status(201).json({ token, username: user.username });
   } catch (err) {
     return next(err);
   }
