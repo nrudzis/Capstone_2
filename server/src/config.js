d25d1d67a8f4cd784d3b75f67af6607d25b81289
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-05 15:42:26 -0700

    add SECRET_KEY to config

--- server/src/config.js
+++ server/src/config.js
@@ -4,6 +4,8 @@ require("dotenv/config");
 
 const PORT = +process.env.PORT || 3001;
 
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 const API_BASE_URL_1 = "https://data.alpaca.markets";
@@ -17,6 +19,7 @@ function getDatabaseUri() {
 
 module.exports = {
   PORT,
+  SECRET_KEY,
   BCRYPT_WORK_FACTOR,
   API_BASE_URL_1,
   API_BASE_URL_2,
