2d771aa11087e9ad4f207f7032f4dd92a520a579
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-05 16:55:22 -0700

    uncomment showing all users in App component since it is now protected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useLoaderData } from 'react-router-dom'
+//import { useLoaderData } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const users = useLoaderData();
+  //const users = useLoaderData();
 
   return (
     <>
@@ -31,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p>Users: ${JSON.stringify(users)}</p>
+      {/*<p>Users: ${JSON.stringify(users)}</p>*/}
     </>
   )
 }
