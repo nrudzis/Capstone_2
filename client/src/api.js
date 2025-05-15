db2198c83ff54d00ce8c46a48ee9f71bd95b0fb3
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-15 02:03:01 -0700

    refactor error handling with generic method in client api

--- client/src/api.js
+++ client/src/api.js
@@ -21,63 +21,96 @@ class SwapApi {
       data: method !== "get" ? data : undefined,
     };
 
-    try {
-      return await axios(config);
-    } catch (err) {
-      console.error("API Error:", err.response);
-      const message = err.response || "An unknown error occured.";
-      return { success: false, error: message }
-    }
+    return await axios(config);
+  }
+
+  /** Generic method to handle api errors. */
+  static handleError(err) {
+    const message = err.response.data.error.message || "An unknown error occured.";
+    console.error(`API Error: ${message}`);
+    return { success: false, error: message }
   }
 
   /** Log in a user. */
   static async login(data) {
-    const res = await this.request("auth/token", data, "post");
-    if (res.data.token) localStorage.setItem("token", res.data.token);
-    return { success: true, username: res.data.username };
+    try {
+      const res = await this.request("auth/token", data, "post");
+      if (res.data.token) localStorage.setItem("token", res.data.token);
+      return { success: true, username: res.data.username };
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Register a new user. */
   static async register(data) {
-    const res = await this.request("auth/register", data, "post");
-    if (res.data.token) localStorage.setItem("token", res.data.token);
-    return { success: true, username: res.data.username }
+    try {
+      const res = await this.request("auth/register", data, "post");
+      if (res.data.token) localStorage.setItem("token", res.data.token);
+      return { success: true, username: res.data.username }
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Get details on a user. */
   static async getUser(username) {
-    const res = await this.request(`users/${username}`);
-    return res.data.user;
+    try {
+      const res = await this.request(`users/${username}`);
+      return res.data.user;
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Get all the users. */
   static async getAllUsers() {
-    const res = await this.request("users");
-    return res.data.users;
+    try {
+      const res = await this.request("users");
+      return res.data.users;
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Log out user. */
   static logout() {
-    localStorage.removeItem("token");
-    return { success: true, message: "Log out success." };
+    try {
+      localStorage.removeItem("token");
+      return { success: true, message: "Log out success." };
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Fund user's account. */
   static async fundAccount(username) {
-    await this.request(`users/${username}/fund-account`, {}, "post");
-    return { success: true, message: "Fund account success." };
+    try {
+      await this.request(`users/${username}/fund-account`, {}, "post");
+      return { success: true, message: "Fund account success." };
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 
   /** Send funds to another user. */
   static async sendFunds(usernameSending, data) {
-    await this.request(`users/${usernameSending}/send-funds`, data, "post");
-    return { success: true, message: "Send funds success." }
+    try {
+      await this.request(`users/${usernameSending}/send-funds`, data, "post");
+      return { success: true, message: "Send funds success." }
+    } catch (err) {
+      return this.handleError(err)
+    }
   }
 
   /** Buy/sell assets. */
   static async marketTransaction(username, data) {
-    await this.request(`users/${username}/market-transaction`, data, "post")
-    return { success: true, message: "Transaction success." }
+    try {
+      await this.request(`users/${username}/market-transaction`, data, "post")
+      return { success: true, message: "Transaction success." }
+    } catch (err) {
+      return this.handleError(err);
+    }
   }
 }
 
