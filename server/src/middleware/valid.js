cc3937301c2e4c3313131e6df9109d00fcb45908
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-17 15:41:23 -0700

    move schema validation logic to middleware

---
+++ server/src/middleware/valid.js
@@ -0,0 +1,27 @@
+/** Middleware to handle json schema validation in routes. */
+
+const jsonschema = require("jsonschema");
+const { ExpressError } = require("../expressError");
+
+
+/** Validate schema.
+ */
+
+function validateSchema(schema) {
+  return (req, res, next) => {
+    try {
+      const result = jsonschema.validate(req.body, schema);
+      if (!result.valid) {
+        const errorStack = result.errors.map(err => err.stack);
+        throw new ExpressError(errorStack, 400);
+      }
+      return next();
+    } catch (err) {
+      return next(err);
+    }
+  };
+}
+
+module.exports = {
+  validateSchema,
+};
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,21 +4,15 @@ const { createToken } = require("../helpers/token.js");
 
 const express = require("express");
 const User = require("../models/user");
-const { ExpressError } = require("../expressError");
 
 const router = express.Router();
-const jsonschema = require("jsonschema");
+const { validateSchema } = require("../middleware/valid");
 const userSchema = require("../schemas/userSchema.json");
 
 
 /** User model check: authenticate */
-router.post("/token", async (req, res, next) => {
+router.post("/token", validateSchema(userSchema), async (req, res, next) => {
   try {
-    const result = jsonschema.validate(req.body, userSchema);
-    if (!result.valid) {
-      const errorStack = result.errors.map(err => err.stack);
-      throw new ExpressError(errorStack, 400);
-    }
     const user = await User.authenticate(req.body);
     const token = createToken(user);
     return res.json({ token, username: user.username });
@@ -28,13 +22,8 @@ router.post("/token", async (req, res, next) => {
 });
 
 /** User model check 3: register */
-router.post("/register", async (req, res, next) => {
+router.post("/register", validateSchema(userSchema), async (req, res, next) => {
   try {
-    const result = jsonschema.validate(req.body, userSchema);
-    if (!result.valid) {
-      const errorStack = result.errors.map(err => err.stack);
-      throw new ExpressError(errorStack, 400);
-    }
     const user = await User.register(req.body);
     const token = createToken(user);
     return res.status(201).json({ token, username: user.username });
