e183e10a849ad2c393342c9c3a7f72eb7a48853f
Nick Rudzis <nick.rudzis@gmail.com>
2025-05-16 21:35:22 -0700

    improve Login and Register forms with MUI and client-side validation

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 import { useNavigate, useOutletContext, Link } from 'react-router'
 import SwapApi from './api.js'
 
 function Login() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: ""
-  });
 
   const navigate = useNavigate();
   const { showToast } = useOutletContext();
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
+  const {
+    control,
+    handleSubmit
+  } = useForm({
+    username: "",
+    password: "",
+  });
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const result = await SwapApi.login(formData);
+  const onSubmit = async (data) => {
+    const result = await SwapApi.login(data);
     if (result.success) {
       navigate(`/users/${result.username}`);
       showToast(`Successfully logged in. Welcome, ${result.username}!`);
@@ -32,33 +33,78 @@ function Login() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password" 
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Log In</button>
-      </form>
+      <Card>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" sx={{ display: "flex", justifyContent: "flex-start" }}>
+              Log In
+            </Typography>
+            <Controller
+              name="username"
+              control={control}
+              placeholder="username"
+              render={({field, fieldState}) => (
+                <TextField 
+                  fullWidth
+                  sx={{ mt: 2, mb: 1 }}
+                  size="small"
+                  id="username"
+                  label="Username"
+                  variant="outlined"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                  {...field}
+                />
+              )}
+              rules={{
+                required: "Username is required",
+                maxLength: {
+                  value: 20,
+                  message: "Usernames cannot be more than 20 characters"
+                }
+              }}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({field, fieldState}) => (
+                <TextField 
+                  fullWidth
+                  sx={{ mt: 2, mb: 1 }}
+                  size="small"
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                  {...field}
+                />
+              )}
+              rules={{
+                required: "Password is required",
+                maxLength: {
+                  value: 20,
+                  message: "Password cannot be more than 20 characters"
+                }
+              }}
+            />
+          </CardContent>
+          <CardActions sx={{ mr: 1, justifyContent: "flex-end" }}>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ mt: 2, mb: 1 }}
+            >
+              Log In
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
       <br/>
       <p>Don't have an account? Click <Link to="/auth/register">here</Link> to register.</p>
     </>
   )
-}
+};
 
 export default Login;
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 import { useNavigate, useOutletContext, Link } from 'react-router'
 import SwapApi from './api.js'
 
 function Register() {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: ""
-  });
 
   const navigate = useNavigate();
   const { showToast } = useOutletContext();
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
+  const {
+    control,
+    handleSubmit
+  } = useForm({
+    username: "",
+    password: "",
+  });
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const result = await SwapApi.register(formData);
+  const onSubmit = async (data) => {
+    const result = await SwapApi.register(data);
     if (result.success) {
       navigate(`/users/${result.username}`);
       showToast(`Successfully registered. Welcome, ${result.username}!`);
@@ -32,33 +33,78 @@ function Register() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password" 
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      <Card>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" sx={{ display: "flex", justifyContent: "flex-start" }}>
+              Register
+            </Typography>
+            <Controller
+              name="username"
+              control={control}
+              placeholder="username"
+              render={({field, fieldState}) => (
+                <TextField 
+                  fullWidth
+                  sx={{ mt: 2, mb: 1 }}
+                  size="small"
+                  id="username"
+                  label="Username"
+                  variant="outlined"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                  {...field}
+                />
+              )}
+              rules={{
+                required: "Username is required",
+                maxLength: {
+                  value: 20,
+                  message: "Username cannot be more than 20 characters"
+                }
+              }}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({field, fieldState}) => (
+                <TextField 
+                  fullWidth
+                  sx={{ mt: 2, mb: 1 }}
+                  size="small"
+                  id="password"
+                  label="Password"
+                  type="password"
+                  variant="outlined"
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                  {...field}
+                />
+              )}
+              rules={{
+                required: "Password is required",
+                maxLength: {
+                  value: 20,
+                  message: "Password cannot be more than 20 characters"
+                }
+              }}
+            />
+          </CardContent>
+          <CardActions sx={{ mr: 1, justifyContent: "flex-end" }}>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ mt: 2, mb: 1 }}
+            >
+              Register
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
       <br/>
-      <p>Already have an account? Click <Link to="/auth/login">here</Link> to log in.</p>
+      <p>Already have an account? Click <Link to="/auth/login">here</Link> to login.</p>
     </>
   )
-}
+};
 
 export default Register;
