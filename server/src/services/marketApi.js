06e1ee1496f1599e1fbaa621ba7620a51310d8c4
Nick Rudzis <nick.rudzis@gmail.com>
2025-06-04 22:35:04 -0700

    add error logic for marketApi request method

--- server/src/services/marketApi.js
+++ server/src/services/marketApi.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const { API_BASE_URL_1 } = require("../config.js");
 const { API_BASE_URL_2 } = require("../config.js");
-//import { NotFoundError, BadRequestError } from "../expressError.js"
 
 
 /** Related functions for external market api. */
@@ -30,8 +29,10 @@ class MarketApi {
     try {
       return await axios(config);
     } catch (err) {
-      console.error("API Error:", err.response);
-      // TODO: Implement handling logic
+      const status = err.response?.status;
+      const message = err.response?.data?.message || err.message;
+      console.error("API Error:", { status, message });
+      throw new Error(`Request to ${endpoint} failed with status ${status}: ${message}`)
     }
   }
 
@@ -120,80 +121,3 @@ class MarketApi {
 }
 
 module.exports = MarketApi;
-
-
-
-  //  EXAMPLE (crypto and stock):
-  //  {
-  //    "id": "0515189d-1933-4a94-89ce-4e9a24356d58",
-  //    "class": "crypto",                              - this
-  //    "exchange": "CRYPTO",
-  //    "symbol": "AVAX/USD",                           - this
-  //    "name": "Avalanche / US Dollar",                - this
-  //    "status": "active",
-  //    "tradable": true,
-  //    "marginable": false,
-  //    "maintenance_margin_requirement": 100,
-  //    "margin_requirement_long": "100",
-  //    "margin_requirement_short": "100",
-  //    "shortable": false,
-  //    "easy_to_borrow": false,
-  //    "fractionable": true,
-  //    "attributes": [],
-  //    "min_order_size": "0.047528517",
-  //    "min_trade_increment": "0.000000001",
-  //    "price_increment": "0.0005"
-  //  }
-  //
-  //  {
-  //    "id": "b0b6dd9d-8b9b-48a9-ba46-b9d54906e415",
-  //    "class": "us_equity",                           - this
-  //    "exchange": "NASDAQ",
-  //    "symbol": "AAPL",                               - this
-  //    "name": "Apple Inc. Common Stock",              - this
-  //    "status": "active",
-  //    "tradable": true,
-  //    "marginable": true,
-  //    "maintenance_margin_requirement": 30,
-  //    "margin_requirement_long": "30",
-  //    "margin_requirement_short": "30",
-  //    "shortable": true,
-  //    "easy_to_borrow": true,
-  //    "fractionable": true,
-  //    "attributes": [
-  //      "fractional_eh_enabled",
-  //      "has_options"
-  //    ]
-  //  }
-
-  //  EXAMPLE (AAPL):
-  //  {
-  //    "quote": {
-  //      "ap": 252.3,
-  //      "as": 1,
-  //      "ax": "V",
-  //      "bp": 233,
-  //      "bs": 1,
-  //      "bx": "V",
-  //      "c": [
-  //        "R"
-  //      ],
-  //      "t": "2025-02-27T20:59:59.431621087Z",
-  //      "z": "C"
-  //    },
-  //    "symbol": "AAPL"
-  //  }
-
-  //  EXAMPLE (BTC/USD):
-  //  {
-  //    "quotes": {
-  //      "BTC/USD": {
-  //        "ap": 80598.791,
-  //        "as": 0.805028,
-  //        "bp": 80495.293,
-  //        "bs": 0.807003,
-  //        "t": "2025-02-28T04:55:30.361113296Z"
-  //      }
-  //    }
-  //  }
-
